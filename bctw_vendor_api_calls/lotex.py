8fcdbfb12d52ee122ca705cacc53b6a128edfb0c
Paul Parratt <paulparratt@gmail.com>
2020-04-29 11:05:01 -0700

    Fixed truncate statement

--- bctw_vendor_api_calls/lotex.py
+++ bctw_vendor_api_calls/lotex.py
@@ -94,8 +94,8 @@ def lotex_truncate_tables():
         if cursor:
             sql_string = 'TRUNCATE TABLE api_lotex_device_info,' \
                          'api_lotex_device_position_data,' \
-                         'api_lotex_devices_by_user' \
-                         'api_lotex_collar_data' \
+                         'api_lotex_devices_by_user,' \
+                         'api_lotex_collar_data,' \
                          'api_lotex_lp;'
             try:
                 cursor.execute(sql_string)
