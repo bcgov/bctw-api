21c57fd17b6ba9388a730f13d3cc8dbca9902699
ronald-garcia <ronald.garcia@gmail.com>
2020-07-08 00:27:58 -0700

    updating CI/CD - Jenkins using OpenShift scripts

---
+++ openshift/settings.sh
@@ -0,0 +1,7 @@
+export PROJECT_NAMESPACE="dgsbmb"
+export GIT_URI="https://github.com/bcgov/bctw-api.git"
+export GIT_REF="master"
+
+# The templates that should not have their GIT referances(uri and ref) over-ridden
+# Templates NOT in this list will have they GIT referances over-ridden with the values of GIT_URI and GIT_REF
+#export skip_git_overrides="cas-api-build.json"
