1a213c0f90a8f2e2b578c93c98ea9be59d684873
Jamie Popkin <popkinj@littleearth.ca>
2020-07-15 14:28:44 -0700

    insert sql logic
    half way there

--- api/collect/vectronics.js
+++ api/collect/vectronics.js
@@ -21,7 +21,13 @@ const getAllCollars = function () {
     if (err) {
       return console.error('Failed to fetch Vectronics collars: ',err);
     }
-    async.concatSeries(data.rows,getCollarRecords,insertCollarRecords);
+
+    /*****************************************/
+    testing = data.rows.slice(0,1);
+    async.concatSeries(testing,getCollarRecords,insertCollarRecords);
+    /*****************************************/
+
+    // async.concatSeries(data.rows,getCollarRecords,insertCollarRecords);
   };
 
   pgPool.query(sql,done);
@@ -53,6 +59,116 @@ const insertCollarRecords = function(err,result) {
   const records = result
     .map((e) => {return e.body;})
     .flat();
+
+    let sql = `
+      insert into vectronics_collar_data (
+        "idposition",
+        "idcollar",
+        "acquisitiontime",
+        "scts",
+        "origincode",
+        "ecefx",
+        "ecefy",
+        "ecefz",
+        "latitude",
+        "longitude",
+        "height",
+        "dop",
+        "idfixtype",
+        "positionerror",
+        "satcount",
+        "ch01satid",
+        "ch01satcnr",
+        "ch02satid",
+        "ch02satcnr",
+        "ch03satid",
+        "ch03satcnr",
+        "ch04satid",
+        "ch04satcnr",
+        "ch05satid",
+        "ch05satcnr",
+        "ch06satid",
+        "ch06satcnr",
+        "ch07satid",
+        "ch07satcnr",
+        "ch08satid",
+        "ch08satcnr",
+        "ch09satid",
+        "ch09satcnr",
+        "ch10satid",
+        "ch10satcnr",
+        "ch11satid",
+        "ch11satcnr",
+        "ch12satid",
+        "ch12satcnr",
+        "idmortalitystatus",
+        "activity",
+        "mainvoltage",
+        "backupvoltage",
+        "temperature",
+        "transformedx",
+        "transformedy"
+      ) values 
+    `;
+
+    // TODO: Cycle through the records and append the `sql` string with the following
+    `(
+      ${idPosition},
+      ${idCollar},
+      ${acquisitionTime},
+      ${scts},
+      ${originCode},
+      ${ecefX},
+      ${ecefY},
+      ${ecefZ},
+      ${latitude},
+      ${longitude},
+      ${height},
+      ${dop},
+      ${idFixType},
+      ${positionError},
+      ${satCount},
+      ${ch01SatId},
+      ${ch01SatCnr},
+      ${ch02SatId},
+      ${ch02SatCnr},
+      ${ch03SatId},
+      ${ch03SatCnr},
+      ${ch04SatId},
+      ${ch04SatCnr},
+      ${ch05SatId},
+      ${ch05SatCnr},
+      ${ch06SatId},
+      ${ch06SatCnr},
+      ${ch07SatId},
+      ${ch07SatCnr},
+      ${ch08SatId},
+      ${ch08SatCnr},
+      ${ch09SatId},
+      ${ch09SatCnr},
+      ${ch10SatId},
+      ${ch10SatCnr},
+      ${ch11SatId},
+      ${ch11SatCnr},
+      ${ch12SatId},
+      ${ch12SatCnr},
+      ${idMortalityStatus},
+      ${activity},
+      ${mainVoltage},
+      ${backupVoltage},
+      ${temperature},
+      ${transformedX},
+      ${transformedY}
+    )`;
+
+
+    // let sql = `
+    //   INSERT INTO public."Item" ("Id", name)
+    //   VALUES 
+    //     ('1', 'name1'),
+    //     ('2', 'name2'),
+    //     ('3','name3')
+    // `;
 
   console.log(records);
 
