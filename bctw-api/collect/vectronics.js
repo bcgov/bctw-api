1e34cfc8cf0b4c3006439fe775d95eefb2009d99
Jamie Popkin <popkinj@littleearth.ca>
2020-07-18 16:19:54 -0700

    date queries

--- bctw-api/collect/vectronics.js
+++ bctw-api/collect/vectronics.js
@@ -1,6 +1,7 @@
 const pg = require('pg'); // Postgres
 const async = require('async'); // Async management
 const needle = require('needle'); // HTTP requests
+const moment = require('moment'); // Time calculation
 
 const isProd = process.env.NODE_ENV === 'production' ? true : false;
 
@@ -11,6 +12,8 @@ const pgPool = new pg.Pool({
   password: process.env.POSTGRES_PASSWORD,
   host: isProd ? process.env.POSTGRES_SERVER_HOST : 'localhost',
   port: isProd ? process.env.POSTGRES_SERVER_PORT : 5432,
+  // idleTimeoutMillis: 30000,
+  // connectionTimeoutMillis: 60000,
   max: 10
 });
 
@@ -23,11 +26,11 @@ const getAllCollars = function () {
     }
 
     /*****************************************/
-    testing = data.rows.slice(0,1);
-    async.concatSeries(testing,getCollarRecords,insertCollarRecords);
+    // testing = data.rows.slice(0,1);
+    // async.concatSeries(testing,getCollarRecords,insertCollarRecords);
     /*****************************************/
 
-    // async.concatSeries(data.rows,getCollarRecords,insertCollarRecords);
+    async.concatSeries(data.rows,getCollarRecords,insertCollarRecords);
   };
 
   pgPool.query(sql,done);
@@ -37,14 +40,13 @@ const getCollarRecords = function(collar, callback) {
   const apiUrl = process.env.VECTRONICS_URL
   const key = collar.collarkey;
   const id = collar.idcollar
-  const url = `${apiUrl}/${id}/gps?collarkey=${key}`;
-  // console.log(collar);
+  const weekAgo = moment().subtract(7,'d').format('YYYY-MM-DDTHH:mm:ss');
+  const url = `${apiUrl}/${id}/gps?collarkey=${key}&afterScts=${weekAgo}`;
+
+
   console.log(`Fetching data for ${id}`);
-  // console.log(url);
-  // needle.get(url,function (err,res) {
-  //   callback(null,res.body)
-  // });
-  needle.get(url,callback);
+
+  needle.get(url,(err,res,body) => {callback(err,body)});
 };
 
 const insertCollarRecords = function(err,result) {
@@ -53,126 +55,134 @@ const insertCollarRecords = function(err,result) {
     return console.error("Error fetching collar data: ",err);
   }
 
-  // What is return is an array of arrays
-  // console.log(result.map((e) => {return e.body;}));
-
   const records = result
-    .map((e) => {return e.body;})
-    .flat();
+    .flat()
+    .filter((e) => { return e && e.idPosition});
 
-    let sql = `
-      insert into vectronics_collar_data (
-        "idposition",
-        "idcollar",
-        "acquisitiontime",
-        "scts",
-        "origincode",
-        "ecefx",
-        "ecefy",
-        "ecefz",
-        "latitude",
-        "longitude",
-        "height",
-        "dop",
-        "idfixtype",
-        "positionerror",
-        "satcount",
-        "ch01satid",
-        "ch01satcnr",
-        "ch02satid",
-        "ch02satcnr",
-        "ch03satid",
-        "ch03satcnr",
-        "ch04satid",
-        "ch04satcnr",
-        "ch05satid",
-        "ch05satcnr",
-        "ch06satid",
-        "ch06satcnr",
-        "ch07satid",
-        "ch07satcnr",
-        "ch08satid",
-        "ch08satcnr",
-        "ch09satid",
-        "ch09satcnr",
-        "ch10satid",
-        "ch10satcnr",
-        "ch11satid",
-        "ch11satcnr",
-        "ch12satid",
-        "ch12satcnr",
-        "idmortalitystatus",
-        "activity",
-        "mainvoltage",
-        "backupvoltage",
-        "temperature",
-        "transformedx",
-        "transformedy"
-      ) values 
-    `;
+  let sqlPreamble = `
+    insert into vectronics_collar_data (
+      "idposition",
+      "idcollar",
+      "acquisitiontime",
+      "scts",
+      "origincode",
+      "ecefx",
+      "ecefy",
+      "ecefz",
+      "latitude",
+      "longitude",
+      "height",
+      "dop",
+      "idfixtype",
+      "positionerror",
+      "satcount",
+      "ch01satid",
+      "ch01satcnr",
+      "ch02satid",
+      "ch02satcnr",
+      "ch03satid",
+      "ch03satcnr",
+      "ch04satid",
+      "ch04satcnr",
+      "ch05satid",
+      "ch05satcnr",
+      "ch06satid",
+      "ch06satcnr",
+      "ch07satid",
+      "ch07satcnr",
+      "ch08satid",
+      "ch08satcnr",
+      "ch09satid",
+      "ch09satcnr",
+      "ch10satid",
+      "ch10satcnr",
+      "ch11satid",
+      "ch11satcnr",
+      "ch12satid",
+      "ch12satcnr",
+      "idmortalitystatus",
+      "activity",
+      "mainvoltage",
+      "backupvoltage",
+      "temperature",
+      "transformedx",
+      "transformedy",
+      "geom"
+    ) values
+  `;
 
-    // TODO: Cycle through the records and append the `sql` string with the following
-    `(
-      ${idPosition},
-      ${idCollar},
-      ${acquisitionTime},
-      ${scts},
-      ${originCode},
-      ${ecefX},
-      ${ecefY},
-      ${ecefZ},
-      ${latitude},
-      ${longitude},
-      ${height},
-      ${dop},
-      ${idFixType},
-      ${positionError},
-      ${satCount},
-      ${ch01SatId},
-      ${ch01SatCnr},
-      ${ch02SatId},
-      ${ch02SatCnr},
-      ${ch03SatId},
-      ${ch03SatCnr},
-      ${ch04SatId},
-      ${ch04SatCnr},
-      ${ch05SatId},
-      ${ch05SatCnr},
-      ${ch06SatId},
-      ${ch06SatCnr},
-      ${ch07SatId},
-      ${ch07SatCnr},
-      ${ch08SatId},
-      ${ch08SatCnr},
-      ${ch09SatId},
-      ${ch09SatCnr},
-      ${ch10SatId},
-      ${ch10SatCnr},
-      ${ch11SatId},
-      ${ch11SatCnr},
-      ${ch12SatId},
-      ${ch12SatCnr},
-      ${idMortalityStatus},
-      ${activity},
-      ${mainVoltage},
-      ${backupVoltage},
-      ${temperature},
-      ${transformedX},
-      ${transformedY}
-    )`;
+  let values = [];
+  for (const p of records) {
+    values.push(
+      `(
+        ${p.idPosition},
+        ${p.idCollar},
+        '${p.acquisitionTime}',
+        '${p.scts}',
+        '${p.originCode}',
+        ${p.ecefX},
+        ${p.ecefY},
+        ${p.ecefZ},
+        ${p.latitude},
+        ${p.longitude},
+        ${p.height},
+        ${p.dop},
+        ${p.idFixType},
+        ${p.positionError},
+        ${p.satCount},
+        ${p.ch01SatId},
+        ${p.ch01SatCnr},
+        ${p.ch02SatId},
+        ${p.ch02SatCnr},
+        ${p.ch03SatId},
+        ${p.ch03SatCnr},
+        ${p.ch04SatId},
+        ${p.ch04SatCnr},
+        ${p.ch05SatId},
+        ${p.ch05SatCnr},
+        ${p.ch06SatId},
+        ${p.ch06SatCnr},
+        ${p.ch07SatId},
+        ${p.ch07SatCnr},
+        ${p.ch08SatId},
+        ${p.ch08SatCnr},
+        ${p.ch09SatId},
+        ${p.ch09SatCnr},
+        ${p.ch10SatId},
+        ${p.ch10SatCnr},
+        ${p.ch11SatId},
+        ${p.ch11SatCnr},
+        ${p.ch12SatId},
+        ${p.ch12SatCnr},
+        ${p.idMortalityStatus},
+        ${p.activity},
+        ${p.mainVoltage},
+        ${p.backupVoltage},
+        ${p.temperature},
+        ${p.transformedX},
+        ${p.transformedY},
+        st_setSrid(st_point(${p.longitude},${p.latitude}),4326)
+        )`
+    );
+  }
+
+  const sqlPostamble = ' on conflict (idPosition) do nothing';
 
 
-    // let sql = `
-    //   INSERT INTO public."Item" ("Id", name)
-    //   VALUES 
-    //     ('1', 'name1'),
-    //     ('2', 'name2'),
-    //     ('3','name3')
-    // `;
+  sql = sqlPreamble + values.join(',') + sqlPostamble;
 
-  console.log(records);
+  console.log('Possibly entering ' + values.length + ' records');
 
-  pgPool.end();
+
+  const done = function (err) {
+    if (err) {
+      return console.error('Failed to insert records: ',err);
+    }
+    pgPool.end();
+  }
+
+  pgPool.query(sql,done);
+
 }
 
 getAllCollars();
