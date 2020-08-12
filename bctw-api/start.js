b58bd35d883609435fdf67369cbb1d98278f5686
Jamie Popkin <popkinj@littleearth.ca>
2020-08-12 06:35:57 -0700

    Critter end point

--- bctw-api/start.js
+++ bctw-api/start.js
@@ -18,6 +18,86 @@ const pgPool = new pg.Pool({
   max: 10
 });
 
+
+/* ## getDBCritters
+  @param req {object} Node/Express request object
+  @param res {object} Node/Express response object
+  @param next {function} Node/Express function for flow control
+ */
+const getDBCritters = function (req, res, next) {
+  const sql = `
+    with pings as (
+    select
+      geom "geom",
+      recdatetime "date_recorded",
+      deviceid "device_id",
+      'Lotek' "device_vendor"
+    from
+      lotek_collar_data
+    where
+      recdatetime > (current_date - INTERVAL '1 months') and
+      st_isValid(geom)
+
+    union
+
+    select
+      geom "geom",
+      scts "date_recorded",
+      idcollar "device_id",
+      'Vectronics' "device_vendor"
+    from
+      vectronics_collar_data
+    where
+      scts > (current_date - INTERVAL '1 months') and
+      st_isValid(geom)
+    ),
+
+    ping_plus as (
+      select
+        c.species "species",
+        c.caribou_population_unit "population_unit",
+        c.animal_id "animal_id",
+        c.animal_status "animal_status",
+        c.life_stage "live_stage",
+        c.calf_at_heel "calf_at_heel",
+        c.radio_frequency "radio_frequency",
+        c.satellite_network "satellite_network",
+        p.device_id "device_id",
+        p.date_recorded "date_recorded",
+        p.device_vendor "device_vendor",
+        p.geom "geom",
+        ROW_NUMBER() OVER (ORDER BY 1) as id
+      from
+        pings p,
+        caribou_critter c
+      where
+        p.device_id = c.device_id and
+        p.device_vendor = c.collar_make
+    )
+
+    select jsonb_build_object(
+      'type',       'Feature',
+      'id',         id,
+      'geometry',   ST_AsGeoJSON(geom)::jsonb,
+      'properties', to_jsonb(row) - 'id'  - 'geom'
+    ) FROM (SELECT * FROM ping_plus) row;
+  `
+  const done = function (err,data) {
+    if (err) {
+      return res.status(500).send(`Failed to query database: ${err}`);
+    }
+    features = data.rows.map(row => row.jsonb_build_object);
+    featureCollection = {
+      type: "FeatureCollection",
+      features: features
+    };
+
+    res.send(featureCollection);
+  };
+  pgPool.query(sql,done);
+};
+
+
 /* ## getDBCollars
   Get collar data from the database. Returns GeoJSON through Express.
   @param req {object} Node/Express request object
@@ -68,6 +148,7 @@ const app = express()
   .use(cors())
   // .use(compression())
   .get('/get-collars', getDBCollars)
+  .get('/get-critters',getDBCritters)
   .get('*', notFound);
 
 http.createServer(app).listen(3000);
