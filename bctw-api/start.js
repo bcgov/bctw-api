bd276ae75317af16cf0f04e2f31b8756b6dd033a
Jamie Popkin <popkinj@littleearth.ca>
2020-08-14 16:40:56 -0700

    get-critters is fast now

--- bctw-api/start.js
+++ bctw-api/start.js
@@ -26,67 +26,19 @@ const pgPool = new pg.Pool({
  */
 const getDBCritters = function (req, res, next) {
   const sql = `
-    with pings as (
     select
-      geom "geom",
-      recdatetime "date_recorded",
-      deviceid "device_id",
-      'Lotek' "device_vendor"
+      geojson
     from
-      lotek_collar_data
+      vendor_merge
     where
-      recdatetime > (current_date - INTERVAL '2 weeks') and
-      st_isValid(geom)
+      date_recorded > (current_date - INTERVAL '1 months')
+  `;
 
-    union
-
-    select
-      geom "geom",
-      scts "date_recorded",
-      idcollar "device_id",
-      'Vectronics' "device_vendor"
-    from
-      vectronics_collar_data
-    where
-      scts > (current_date - INTERVAL '2 weeks') and
-      st_isValid(geom)
-    ),
-
-    ping_plus as (
-      select
-        c.species "species",
-        c.caribou_population_unit "population_unit",
-        c.animal_id "animal_id",
-        c.animal_status "animal_status",
-        c.life_stage "live_stage",
-        c.calf_at_heel "calf_at_heel",
-        c.radio_frequency "radio_frequency",
-        c.satellite_network "satellite_network",
-        p.device_id "device_id",
-        p.date_recorded "date_recorded",
-        p.device_vendor "device_vendor",
-        p.geom "geom",
-        ROW_NUMBER() OVER (ORDER BY 1) as id
-      from
-        pings p,
-        caribou_critter c
-      where
-        p.device_id = c.device_id and
-        p.device_vendor = c.collar_make
-    )
-
-    select jsonb_build_object(
-      'type',       'Feature',
-      'id',         id,
-      'geometry',   ST_AsGeoJSON(geom)::jsonb,
-      'properties', to_jsonb(row) - 'id'  - 'geom'
-    ) FROM (SELECT * FROM ping_plus) row;
-  `
   const done = function (err,data) {
     if (err) {
       return res.status(500).send(`Failed to query database: ${err}`);
     }
-    features = data.rows.map(row => row.jsonb_build_object);
+    features = data.rows.map(row => row.geojson);
     featureCollection = {
       type: "FeatureCollection",
       features: features
