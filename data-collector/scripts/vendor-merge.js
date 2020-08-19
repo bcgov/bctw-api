288c096a81438618fe0372298f2a4276f30dba41
Jamie Popkin <popkinj@littleearth.ca>
2020-08-19 15:54:24 -0700

    Vendor merge script

---
+++ data-collector/scripts/vendor-merge.js
@@ -0,0 +1,31 @@
+const pg = require('pg'); // Postgres
+const async = require('async'); // Async management
+const needle = require('needle'); // HTTP requests
+const moment = require('moment'); // Time calculation
+
+const isProd = process.env.NODE_ENV === 'production' ? true : false;
+
+// Set up the database pool
+const pgPool = new pg.Pool({
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  host: isProd ? process.env.POSTGRES_SERVER_HOST : 'localhost',
+  port: isProd ? process.env.POSTGRES_SERVER_PORT : 5432,
+  max: 10
+});
+
+
+const callback = (err) => {
+  const now = moment().utc();
+  if (err) {
+    return console.error(`${now}: Merge of vendor tables failed`);
+  } else {
+    return console.log(`${now}: Merge of vendor tables successfull`);
+  }
+};
+
+const sql = '';
+
+
+pgPool.query(sql,callback);
