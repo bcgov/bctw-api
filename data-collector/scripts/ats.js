b44987551b2bc4a34faedc552c7b390ccf3584b5
jordan <jordan.craven@quartech.com>
2021-01-07 12:12:17 -0800

    run ats cypress script as a node spawn to pass env variables via cronjob configuration

---
+++ data-collector/scripts/ats.js
@@ -0,0 +1,26 @@
+ const { spawn } = require("child_process");
+//  require('dotenv').config()
+
+const ATS_URL = process.env.ATS_URL;
+const ATS_PASSWORD = process.env.ATS_PASSWORD;
+const ATS_USERNAME = process.env.ATS_USERNAME;
+
+const envString = `ATS_URL=${ATS_URL},ATS_PASSWORD=${ATS_PASSWORD},ATS_USERNAME=${ATS_USERNAME}`
+
+const cypress = spawn('cypress', ['run', '-b', 'chromium', '--headless', '--env', envString]);
+
+cypress.stdout.on("data", data => {
+    console.log(`stdout: ${data}`);
+});
+
+cypress.stderr.on("data", data => {
+    console.log(`stderr: ${data}`);
+});
+
+cypress.on('error', (error) => {
+    console.log(`error: ${error.message}`);
+});
+
+cypress.on("close", code => {
+    console.log(`child process exited with code ${code}`);
+});
