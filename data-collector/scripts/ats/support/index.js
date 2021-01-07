5d16e3d9b0e9a540dc39e4e62f484ddd5f3e325d
jordan <jordan.craven@quartech.com>
2021-01-07 05:42:53 -0800

    dont import missing file

--- data-collector/scripts/ats/support/index.js
+++ data-collector/scripts/ats/support/index.js
@@ -10,7 +10,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+// import './commands'
 
 before(() => {
   cy.task('cleanDownloads');
