7436f6502dcf09cd6a63c33767ec888fa6ba6ccc
jordan <jordan.craven@quartech.com>
2021-02-24 06:11:47 -0800

    ats debug

--- data-collector/scripts/ats/integration/ats_spec.js
+++ data-collector/scripts/ats/integration/ats_spec.js
@@ -11,6 +11,19 @@ describe('ATS Test', () => {
     ATS_LOGIN_FORM_ID
   } = env;
 
+  it('can visit ATS site', () => {
+    cy.visit(ATS_URL);
+    cy.url().should('include', 'login.aspx')
+  });
+
+  it('can login to ATS site', () => {
+    cy.visit(ATS_URL);
+    cy.get(ATS_USERNAME_FIELD_ID).type(ATS_USERNAME);
+    cy.get(ATS_PASSWORD_FIELD_ID).type(ATS_PASSWORD);
+    cy.get(ATS_LOGIN_FORM_ID).submit();
+    cy.url().should('include', 'home.aspx');
+  });
+
   it('can download transmission data from the ATS site', () => {
     const downloadTransmissionsSelector = createSelector('download all transmissions');
     cy.visit(ATS_URL);
@@ -19,7 +32,6 @@ describe('ATS Test', () => {
     cy.get(ATS_PASSWORD_FIELD_ID).type(ATS_PASSWORD);
     cy.get(ATS_LOGIN_FORM_ID).submit();
 
-    cy.wait(5000);
     // no way to catch this error, so this test WILL fail
     // because Cypress expects a new page to be loaded when the button is clicked
     cy.get(downloadTransmissionsSelector).parent().click();
@@ -37,7 +49,6 @@ describe('ATS Test', () => {
   });
 
   it('can parse downloaded ATS files, and upload the rows to the database', () => {
-    cy.wait(5000);
     // handled in plugins/download.ts
     cy.task('handleParseAndInsert');
   });
