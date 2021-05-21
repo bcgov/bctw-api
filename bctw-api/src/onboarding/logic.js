11c1b88ae43b039dde0b0acbbf042a6db550576a
Jamie Popkin <popkinj@littleearth.ca>
2021-05-21 16:13:35 -0700

    Button and validation working

--- bctw-api/src/onboarding/logic.js
+++ bctw-api/src/onboarding/logic.js
@@ -1,1 +1,12 @@
-console.log('yo')
+const clicked = () => {
+  const input = document.getElementById('icon_prefix')
+  const email = input.value;
+  const re = /^[^\s@]+@[^\s@]+$/; // Match a valid email
+
+  // If successful... Display then blank input 
+  if (re.test(email)) {
+    M.toast({html: 'Your request was sent successfully'})
+    input.value = '';
+  }
+
+}
