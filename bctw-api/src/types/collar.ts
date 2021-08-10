b94571c7e3b4663e1261bf460b183b17fa7d91d2
Victor Chong <62109882+victorchong@users.noreply.github.com>
2021-08-09 17:04:54 -0700

    Clarified user_comment field as device_comment

--- bctw-api/src/types/collar.ts
+++ bctw-api/src/types/collar.ts
@@ -3,10 +3,11 @@ import { BCTWBaseType } from './base_types';
 export interface ICollar extends BCTWBaseType {
   // activation_comment: string;
   // activation_status: boolean;
-  collar_transaction_id: string;
   camera_device_id: number;
-  device_id: number;
+  collar_transaction_id: string;
+  // device_comment: string;
   device_deployment_status: string;
+  device_id: number;
   device_make: string;
   device_malfunction_type: string;
   device_model: string;
@@ -26,30 +27,32 @@ export interface ICollar extends BCTWBaseType {
   retrieval_date: Date;
   retrieved: boolean;
   satellite_network: string;
-  user_comment: string;
+  user_comment: string; // TODO: remove
   vendor_activation_status: boolean; // TODO: remove
 }
+
 export class Collar implements ICollar {
   // activation_comment: string;
   // activation_status: boolean;
+  camera_device_id: number;
   collar_id: string;
   collar_transaction_id: string;
-  camera_device_id: number;
-  device_id: number;
+  // device_comment: string;
   device_deployment_status: string;
+  device_id: number;
   device_make: string;
-  device_status: string;
   device_malfunction_type: string;
   device_model: string;
+  device_status: string;
   device_type: string;
   dropoff_device_id: number;
   dropoff_frequency: number;
   dropoff_frequency_unit: string;
-  frequency: number;
   // first_activation_month: number;
   // first_activation_year: number;
   fix_rate: number;
   fix_success_rate: number;
+  frequency: number;
   frequency_unit: string;
   malfunction_date: Date;
   purchase_comment: string; // TODO: remove
@@ -58,11 +61,12 @@ export class Collar implements ICollar {
   retrieval_date: Date;
   retrieved: boolean;
   satellite_network: string;
-  user_comment: string;
-  vendor_activation_status: boolean; // TODO: remove
+  user_comment: string; // TODO: remove
   valid_from: Date;
   valid_to: Date;
+  vendor_activation_status: boolean; // TODO: remove
 }
+
 interface ChangeCollarData {
   collar_id: string;
   animal_id: string;
