0289c54c0b15cd7152b06d932840373ebf1e10b1
jordan <jordan.craven@quartech.com>
2021-08-19 08:48:36 -0700

    fix dayjs isWithin24Hrs helper

--- data-collector/scripts/ats/plugins/time.ts
+++ data-collector/scripts/ats/plugins/time.ts
@@ -22,7 +22,7 @@ const parseAsLocal = (str: string | undefined): Dayjs => dayjs(str).tz(TZ_PT, tr
  * @returns true if the @param dateToCheck is within 24 hours of the @param start
  */
 const isWithin24Hrs = (start: Dayjs, dateToCheck: Dayjs): boolean => {
-  const nextDay = start.add(dayjs.duration({'days' : 1}));
+  const nextDay = start.add(1, 'day');
   return dateToCheck.isBefore(nextDay);
 }
 
