dce73a4a07540cd5154d9a0a93f319c482fd9c64
Mac Deluca <Mac.Deluca@quartech.com>
2022-03-08 09:38:52 -0800

    Added CORS options to prevent addUser from failing

--- bctw-api/src/server.ts
+++ bctw-api/src/server.ts
@@ -21,6 +21,7 @@ const unauthorizedURLs: Record<string, string> = {
 };
 
 const options: cors.CorsOptions = {
+  origin: '*',
   methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
   preflightContinue: false,
   optionsSuccessStatus: 204,
