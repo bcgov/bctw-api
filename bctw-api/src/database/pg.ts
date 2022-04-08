6f1da7e2083753e2363024027ca906d2893020a2
Mac Deluca <Mac.Deluca@quartech.com>
2022-04-08 14:43:29 -0700

    Created test, enabled hot-reloading for api, added local .env for port

--- bctw-api/src/database/pg.ts
+++ bctw-api/src/database/pg.ts
@@ -2,15 +2,17 @@ import pg, { PoolClient } from 'pg';
 import * as dotenv from 'dotenv';
 
 const isProd = process.env.NODE_ENV === 'production' ? true : false;
+
 if (!isProd) {
   dotenv.config();
 }
-const pgPort = +(isProd ? process.env.POSTGRES_SERVER_PORT ?? 5432 : 5432) as number;
+//Changed the port at the end to 6666 -> this is for local development
+const pgPort = +(process.env.POSTGRES_SERVER_PORT ?? 5432 ) as number;
+//const pgPort = +(isProd ? process.env.POSTGRES_SERVER_PORT ?? 5432 : 5432) as number;
 const pgHost = (isProd ? process.env.POSTGRES_SERVER_HOST : 'localhost') as string;
 // always commit if in production
 const ROLLBACK = (process.env.ROLLBACK === 'true') && !isProd;
 
-console.log('node env:', process.env.NODE_ENV);
 console.log('database port', pgPort);
 console.log('database host', pgHost);
 console.log('rolling back persisting changes', ROLLBACK);
@@ -29,10 +31,10 @@ pgPool.on('error', (err: Error, client: PoolClient): void => {
   console.log(`postgresql error: ${err}`);
 });
 pgPool.on('acquire', (client: PoolClient): void => {
-  // console.log(`postgresql client acquired`);
+  //console.log(`postgresql client acquired`);
 });
 pgPool.on('connect', (client: PoolClient): void => {
-  // console.log(`postgresql client connected`);
+  //console.log(`postgresql client connected`);
 });
 
 export {
