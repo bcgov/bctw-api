a894f69195d4bbf17b1b5375c7c0a0b77ccd544f
Mac Deluca <Mac.Deluca@quartech.com>
2022-08-08 12:25:09 -0700

    Updated vendor merge

--- data-collector/scripts/vendor-merge.ts
+++ data-collector/scripts/vendor-merge.ts
@@ -1,23 +1,38 @@
 import moment from 'moment';
-import { pgPool } from './utils/db';
+import "dotenv/config";
+import { isPoolEmpty, pgPool, queryAsync, safelyDrainPool } from './utils/db';
+import { QueryResult } from 'pg';
 
-const callback = (err) => {
+const callback = (err: Error, result: QueryResult<any>) => {
   const now = moment().utc();
   if (err) {
     return console.error(`${now}: Merge of vendor tables failed`, err);
   } else {
-    return console.log(`${now}: Merge of vendor tables successfull`);
+    return console.log(`${now}: `, result);
   }
 };
+const SQL = [
+  'refresh materialized view vendor_merge_view_no_critter;', 
+  'refresh materialized view latest_transmissions;', 
+  //'refresh materialized view concurrently telemetry_with_security_m;',
+  'call bctw.proc_check_for_missing_telemetry();'
+];
 
 /**
- * updates the materialized views for raw telemetry and latest device transmissions.
+ * updates the materialized views for raw telemetry, latest device transmissions
+ * and telemetry with security.
  * after that is complete, call the procedure to check for device malfunction alerts.
  */
-const sql = `
-  refresh materialized view vendor_merge_view_no_critter;
-  refresh materialized view latest_transmissions;
-  refresh materialized view concurrently telemetry_with_security_m;
-  call bctw.proc_check_for_missing_telemetry();
-`;
-pgPool.query(sql, callback);
+
+const main = async() => {
+  console.log(`Starting Vendor Merge V1.1`);
+
+  for(const S of SQL){
+    await queryAsync(S).then(() =>
+      console.log(`${moment().utc()}: '${S}' Successfully executed.`));
+  }
+  await safelyDrainPool(10);
+  console.log(`Vendor Merge complete.`)
+}
+
+main();
