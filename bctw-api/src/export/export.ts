9a8914f21b97fef2395ff3f14d04677ae5bf7edd
Graham Stewart <graham.stewart@quartech.com>
2022-10-20 14:23:32 -0700

    Removes old version of export-all API endpoint

--- bctw-api/src/export/export.ts
+++ bctw-api/src/export/export.ts
@@ -77,22 +77,21 @@ const getExportData = async function (
   }
 };
 
+/*
+* Called by the new standalone export page. 
+* Allows user to export based on specific column parameters and geographic regions contained in polygon data.
+*/
 const getAllExportData = async function (
   req: Request,
   res: Response
 ): Promise<Response> {
-  /*const sql = constructFunctionQuery(
-    'export_telemetry_with_params', 
-    [getUserIdentifier(req), req.body.keys, req.body.operators, req.body.term, req.body.range.start, req.body.range.end],
-    false,
-    'bctw',
-    true
-  );*/
+
   const idir = getUserIdentifier(req);
   const queries = JSON.stringify(req.body.queries);
   const start = req.body.range.start;
   const end = req.body.range.end;
-  const sql = `SELECT * FROM bctw.export_telemetry_with_params('${idir}', '${queries}', '${start}', '${end}'); `;
+  const polygons = 'ARRAY[ ' + req.body.polygons.map(o => `'${o}'`).join(', ') + ']';
+  const sql = `SELECT * FROM bctw.export_telemetry_with_params('${idir}', '${queries}', '${start}', '${end}', ${polygons}); `;
   console.log(sql);
 
   const { result, error, isError } = await query(
@@ -103,60 +102,6 @@ const getAllExportData = async function (
     return res.status(500).send(error.message);
   }
   return res.send(result.rows);
-};
-
-const getAllExportDataRename = async function (
-  req: Request,
-  res: Response
-): Promise<Response> {
-  // console.log(req.body);
-  /*
-  SELECT row_to_json(t) FROM (
-		SELECT * FROM bctw_dapi_v1.animal_historic_v av 
-		WHERE critter_id = animalid
-		AND tsrange(startdate, enddate) && tsrange(valid_from::timestamp, valid_to::timestamp)
-		ORDER BY valid_to DESC NULLS FIRST*/
-  const base = 
-		`WITH ids AS ( SELECT critter_id FROM UNNEST(bctw.get_user_critter_access('gstewart')) AS critter_id ),
-    assignments AS ( SELECT caav.critter_id, collar_id FROM ids JOIN bctw.collar_animal_assignment_v caav ON ids.critter_id = caav.critter_id )
-    SELECT 
-    a.critter_id, 
-    asg.collar_id, 
-    animal_id, 
-    wlh_id,
-    device_id,
-    species,
-    code_name AS population_unit,
-    latitude,
-    longitude,
-    bc_albers_x,
-    bc_albers_y,
-    acquisition_date,
-    geom
-    FROM 
-    assignments asg JOIN bctw.animal a ON asg.critter_id = a.critter_id 
-    JOIN bctw.telemetry_v tel ON asg.collar_id = tel.collar_id
-    LEFT JOIN code ON population_unit = code.code_id `;
-  const filter = getFilterFromRequest(req, true);
-  const username = getUserIdentifier(req) ;
-  if (!username) {
-    return res.status(500).send('username not provided');
-  }
-  console.log(filter);
-  const sql = constructGetQuery({
-    base,
-    filter: appendFilter(filter, false, false),
-    //order:  [{field: 'valid_to', order: 'desc'}]
-  });
-  console.log(req.body);
-  const finalSql = `${sql} AND acquisition_date <@ tsrange('${req.body.range.start}', '${req.body.range.end}') ORDER BY acquisition_date DESC`;
-  console.log(finalSql);
-  const { result, error, isError } = await query(finalSql);
-  if (isError) {
-    return res.status(500).send(error);
-  }
-  return res.send(result.rows);
-  
 };
 
 export { getExportData, getAllExportData };
