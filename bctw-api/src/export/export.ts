461a08efb81872941ff9578012c48783e1b57f04
Mac Deluca <Mac.Deluca@quartech.com>
2022-10-31 14:16:08 -0700

    Fixed final merge conflict

--- bctw-api/src/export/export.ts
+++ bctw-api/src/export/export.ts
@@ -2,7 +2,12 @@ import { Request, Response } from 'express';
 import { fn_get_critter_history } from '../apis/animal_api';
 import { fn_get_collar_history } from '../apis/collar_api';
 import { S_API, S_BCTW } from '../constants';
-import { appendFilter, constructFunctionQuery, constructGetQuery, query, queryParams } from '../database/query';
+import {
+  appendFilter,
+  constructFunctionQuery,
+  constructGetQuery,
+  query,
+} from '../database/query';
 import { getFilterFromRequest, getUserIdentifier } from '../database/requests';
 
 enum eExportType {
@@ -15,31 +20,36 @@ enum eExportType {
 type MapRange = {
   start: string;
   end: string;
-}
+};
 
 const pg_movement_history = 'get_movement_history';
 
-const makeQuery = (query: string, username: string, id: string, range: MapRange) => {
+const makeQuery = (
+  query: string,
+  username: string,
+  id: string,
+  range: MapRange
+) => {
   const params = [username, id];
   if (range) {
-    params.push(...[range.start, range.end])
+    params.push(...[range.start, range.end]);
   }
   return constructFunctionQuery(query, params, false, S_API);
-}
+};
 
-const movementSQL = (user: string, id: string, range: MapRange): string => 
+const movementSQL = (user: string, id: string, range: MapRange): string =>
   makeQuery(pg_movement_history, user, id, range);
 
-const animalSQL = (user: string, id: string, range: MapRange): string => 
+const animalSQL = (user: string, id: string, range: MapRange): string =>
   makeQuery(fn_get_critter_history, user, id, range);
 
-const deviceSQL = (user: string, id: string, range: MapRange): string => 
+const deviceSQL = (user: string, id: string, range: MapRange): string =>
   makeQuery(fn_get_collar_history, user, id, range);
 
 /**
  * called via the map page -> export view, this endpoint handles requests to
  * export data when that data is not already in UI memory.
- * Primarily animal/device metadata history and animal movement history requests 
+ * Primarily animal/device metadata history and animal movement history requests
  */
 const getExportData = async function (
   req: Request,
@@ -47,57 +57,63 @@ const getExportData = async function (
 ): Promise<Response> {
   // console.log(req.body);
   const { type, range, collar_ids, critter_ids } = req.body;
-  const username = getUserIdentifier(req) ;
+  const username = getUserIdentifier(req);
   if (!username) {
     return res.status(500).send('username not provided');
   }
   const sqlStrings: string[] = [];
   switch (type) {
     case eExportType.animal:
-      critter_ids.forEach(i => sqlStrings.push(animalSQL(username, i, range)))
+      critter_ids.forEach((i) =>
+        sqlStrings.push(animalSQL(username, i, range))
+      );
       break;
     case eExportType.collar:
-      collar_ids.forEach(i => sqlStrings.push(deviceSQL(username, i, range)))
+      collar_ids.forEach((i) => sqlStrings.push(deviceSQL(username, i, range)));
       break;
     case eExportType.movement:
-      collar_ids.forEach(i => sqlStrings.push(movementSQL(username, i, range)))
+      collar_ids.forEach((i) =>
+        sqlStrings.push(movementSQL(username, i, range))
+      );
       break;
   }
-  const promises = sqlStrings.map(s => query(s, ''));
+  const promises = sqlStrings.map((s) => query(s, ''));
   console.log(sqlStrings);
 
   const resolved = await Promise.all(promises);
-  const errors = resolved.filter(r => r.isError);
+  const errors = resolved.filter((r) => r.isError);
   if (errors.length) {
-    const message = errors.map(e => e.error).join();
-    return res.status(500).send(message)
+    const message = errors.map((e) => e.error).join();
+    return res.status(500).send(message);
   } else {
-    const results = resolved.map(r => r.result?.rows.map(o => Object.values(o)[0])) ;
+    const results = resolved.map((r) =>
+      r.result?.rows.map((o) => Object.values(o)[0])
+    );
     return res.send(results);
   }
 };
 
 /*
-* Called by the new standalone export page. 
-* Allows user to export based on specific column parameters and geographic regions contained in polygon data.
-*/
+ * Called by the new standalone export page.
+ * Allows user to export based on specific column parameters and geographic regions contained in polygon data.
+ */
 const getAllExportData = async function (
   req: Request,
   res: Response
 ): Promise<Response> {
-
   const idir = getUserIdentifier(req);
   const queries = JSON.stringify(req.body.queries);
   const start = req.body.range.start;
   const end = req.body.range.end;
-  const polygons = 'ARRAY[ ' + req.body.polygons.map(o => `'${o}'`).join(', ') + ']::text[]';
+  const polygons =
+    'ARRAY[ ' + req.body.polygons.map((o) => `'${o}'`).join(', ') + ']::text[]';
   const sql = `SELECT * FROM bctw.export_telemetry_with_params('${idir}', '${queries}', '${start}', '${end}', ${polygons}); `;
 
   const { result, error, isError } = await query(
     sql,
     'failed to retrieve telemetry'
   );
-  if(isError) {
+  if (isError) {
     return res.status(500).send(error.message);
   }
   return res.send(result.rows);
