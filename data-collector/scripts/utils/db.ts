7197efa081738e94cb5995f28bb31e77885eb20e
Mac Deluca <Mac.Deluca@quartech.com>
2022-11-09 16:02:51 -0800

    Updated cronjobs, added latest_valid_transmissions to vendor-merge script

--- data-collector/scripts/utils/db.ts
+++ data-collector/scripts/utils/db.ts
@@ -1,58 +1,72 @@
-import pg, {QueryResult, Pool } from 'pg';
-import { Dayjs } from 'dayjs';
-import { eVendorType } from './credentials';
-const dayjs = require('dayjs')
+import pg, { QueryResult, Pool } from "pg";
+import { Dayjs } from "dayjs";
+import { eVendorType } from "./credentials";
+const dayjs = require("dayjs");
 
 const TIMEOUT_MILLIS = 5000;
 
-const isProd = process.env.NODE_ENV === 'production' ? true : false;
+const isProd = process.env.NODE_ENV === "production" ? true : false;
 
 // Set up the database pool
 const user = process.env.POSTGRES_USER;
 const database = process.env.POSTGRES_DB;
 const password = process.env.POSTGRES_PASSWORD;
-const host = isProd ? process.env.POSTGRES_SERVER_HOST : 'localhost';
+const host = isProd ? process.env.POSTGRES_SERVER_HOST : "localhost";
 const port = isProd ? +(process?.env?.POSTGRES_SERVER_PORT ?? 5432) : 5432;
 
 //Might need allowExitOnIdel
-const pool = { user, database, password, host, port, max: 10, idleTimeoutMillis: TIMEOUT_MILLIS};
+const pool = {
+  user,
+  database,
+  password,
+  host,
+  port,
+  max: 10,
+  idleTimeoutMillis: TIMEOUT_MILLIS,
+};
 
 const pgPool = new pg.Pool(pool);
 
 const queryAsync = async (sql: string): Promise<QueryResult> => {
   const client = await pgPool.connect();
-  
+
   let data;
   try {
     data = await client.query(sql);
-    await client.query('commit');
-  }
-  catch (e) {
-    await client.query('rollback');
-    console.log(e)
+    await client.query("commit");
+  } catch (e) {
+    await client.query("rollback");
+    console.log(e);
     throw e;
-  }
-  finally {
+  } finally {
     client.release();
   }
   return data;
-}
+};
 
 /**
- * returns the @type {Dayjs} timestamp of the last alert inserted to the alert table of device_make @type {eVendorType} 
+ * returns the @type {Dayjs} timestamp of the last alert inserted to the alert table of device_make @type {eVendorType}
  * if none are found, returns null
  */
-const getLastAlertTimestamp = async (alert_table: string, device_make: eVendorType): Promise<Dayjs | null> => {
+const getLastAlertTimestamp = async (
+  alert_table: string,
+  device_make: eVendorType
+): Promise<Dayjs | null> => {
   const sql = `select valid_from from ${alert_table} where device_make = '${device_make}' order by valid_from desc limit 1`;
   const result = await queryAsync(sql);
-  return result.rowCount > 0 ? dayjs(result.rows[0]['valid_from']) : null;
-}
+  return result.rowCount > 0 ? dayjs(result.rows[0]["valid_from"]) : null;
+};
 
 /**
- * @returns a bool depending on if there is an existing alert present in the 
+ * @returns a bool depending on if there is an existing alert present in the
  * telemetry alert table where the device ID and device make match
  */
-const getIsDuplicateAlert = async (alert_table: string, device_id: number, device_make: eVendorType, alertType: string): Promise<boolean> => {
+const getIsDuplicateAlert = async (
+  alert_table: string,
+  device_id: number,
+  device_make: eVendorType,
+  alertType: string
+): Promise<boolean> => {
   const sql = `
     select device_id from ${alert_table}
     where device_make = '${device_make}'
@@ -67,8 +81,7 @@ const getIsDuplicateAlert = async (alert_table: string, device_id: number, devic
   } else {
     return false;
   }
-  
-}
+};
 
 /**
  * gets lat long for a deviceid
@@ -76,11 +89,15 @@ const getIsDuplicateAlert = async (alert_table: string, device_id: number, devic
  * lotek stopped providing proper coordinates with alerts
  * @returns an latLong
  */
- interface latLong {
+interface latLong {
   latitude: number;
   longitude: number;
 }
- const getLastKnownLatLong = async (device_id: number, device_make: string, alert_time: string): Promise<latLong> => {
+const getLastKnownLatLong = async (
+  device_id: number,
+  device_make: string,
+  alert_time: string
+): Promise<latLong> => {
   const sql = `
   SELECT latitude, longitude
   FROM telemetry
@@ -90,49 +107,69 @@ const getIsDuplicateAlert = async (alert_table: string, device_id: number, devic
   AND longitude != 0 OR NULL
   AND acquisition_date <= '${alert_time}'
   ORDER BY acquisition_date DESC LIMIT 1;
-  `
+  `;
   const result = await queryAsync(sql);
   return result.rows[0];
-}
+};
 
 //Checks if the DB pool is empty of clients
 //Uses timeout to set a wait between requests when called
 //Returns a boolean Promise
 const isPoolEmpty = (): Promise<boolean> => {
-  const {idleCount, waitingCount} = pgPool;
+  const { idleCount, waitingCount } = pgPool;
 
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if(!idleCount && !waitingCount){
+      if (!idleCount && !waitingCount) {
         resolve(true);
-        
-      }else{
-        console.log(`Connections still active... IdleCount: ${pgPool.idleCount} | WaitingCount: ${pgPool.waitingCount}`);
+      } else {
+        console.log(
+          `Connections still active... IdleCount: ${pgPool.idleCount} | WaitingCount: ${pgPool.waitingCount}`
+        );
         resolve(false);
       }
     }, 3000);
   });
-}
-
+};
 
 //Handles closing the db connection safely.
 //Retries: Used as a safe guard for infinite loops.
-const safelyDrainPool = async(retries: number) => {
-  while(true){
-    const isEmpty = await isPoolEmpty().then(res=>res);
-    if(isEmpty) {
-      console.log(`Pool drained successfully.`)
+const safelyDrainAndClosePool = async (retries: number) => {
+  while (true) {
+    const isEmpty = await isPoolEmpty();
+    if (isEmpty) {
+      console.log(`Pool drained successfully.`);
+      await pgPool
+        .end()
+        .then(() => console.log(`Closing database connection...`))
+        .catch((err) =>
+          console.log(`Problem occured when closing pool connection...`, err)
+        );
       break;
     }
-    if(!retries){
-      console.log(`DB tried ${retries} times to close connection and was not successful. Aborting...`);
+    if (!retries) {
+      console.log(
+        `DB tried ${retries} times to close connection and was not successful. Aborting...`
+      );
       break;
-    } 
+    }
     retries--;
-  } 
-}
+  }
+};
 
 // dont commit transaction if not in production
-const transactionify = (sql: string) => isProd ? sql : `begin; ${sql}; rollback;`;
+const transactionify = (sql: string) =>
+  isProd ? sql : `begin; ${sql}; rollback;`;
 
-export { pgPool, isProd, queryAsync, getLastAlertTimestamp, getIsDuplicateAlert, transactionify, getLastKnownLatLong, TIMEOUT_MILLIS, isPoolEmpty, safelyDrainPool }
+export {
+  pgPool,
+  isProd,
+  queryAsync,
+  getLastAlertTimestamp,
+  getIsDuplicateAlert,
+  transactionify,
+  getLastKnownLatLong,
+  TIMEOUT_MILLIS,
+  isPoolEmpty,
+  safelyDrainAndClosePool,
+};
